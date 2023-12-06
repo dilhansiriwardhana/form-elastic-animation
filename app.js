a5b8770161acbbe1cef0aea18edbf23c542ef346
Dilhan <kanchanasiriwardhana1@gmail.com>
2023-12-06 13:38:14 +0530

    validation added

--- app.js
+++ app.js
@@ -61,5 +61,53 @@ form.addEventListener("click", () => {
         });
       }
     }
+
+    // Validation
+    // Name Validation
+    input.addEventListener("input", (e) => {
+      if (e.target.type === "text") {
+        let inputText = e.target.value;
+        if (inputText.length > 2) {
+          colorize("#6391E8", line, placeholder);
+        } else {
+          colorize("#FE8c99", line, placeholder);
+        }
+      }
+      //  Email Validation
+      if (e.target.type === "email") {
+        let valid = validateEmail(e.target.value);
+        if (valid) {
+          colorize("#6391E8", line, placeholder);
+        } else {
+          colorize("#FE8c99", line, placeholder);
+        }
+      }
+
+      //    Validate PHone
+      if (e.target.type === "tel") {
+        let valid = validatePhone(e.target.value);
+        if (valid) {
+          colorize("#6391E8", line, placeholder);
+        } else {
+          colorize("#FE8c99", line, placeholder);
+        }
+      }
+    });
   });
 });
+
+// checking email validation
+
+function validateEmail(email) {
+  let re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+function validatePhone(phone) {
+  let re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+  return re.test(phone);
+}
+
+function colorize(color, line, placeholder) {
+  gsap.to(line, { stroke: color, duration: 0.75 });
+  gsap.to(placeholder, { color: color, duration: 0.75 });
+}
